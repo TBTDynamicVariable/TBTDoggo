9815390efda64c5a4bd95e1022061c853359dc8d
Myles Lewis <mglewis@bu.edu>
2019-02-16 16:43:40 -0500

    Jonathan Committing

---
+++ app/src/main/gen/com/wowwee/chip_android_sampleproject/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.wowwee.chip_android_sampleproject;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
